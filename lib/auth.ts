d3d8c5a6ac93af2b2f18e2571d65c1b66b69a981
Contactsushil <contact.sushilpandey@gmail.com>
2024-08-31 20:21:54 +0530

    corrected phone

--- lib/auth.ts
+++ lib/auth.ts
@@ -34,7 +34,7 @@ export const authOptions = {
                     return {
                         id: existingUser.id.toString(),
                         name: existingUser.name,
-                        email: existingUser.number
+                        email: existingUser.email
                     }
                 }
                 return null;
@@ -52,7 +52,7 @@ export const authOptions = {
                 return {
                     id: user.id.toString(),
                     name: user.name,
-                    email: user.number
+                    email: user.email
                 }
             } catch(e) {
                 console.error(e);
