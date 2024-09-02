ea535cb22bb009c55a2cc9b24b8b528660231591
Contactsushil <contact.sushilpandey@gmail.com>
2024-09-03 00:40:02 +0530

    Graphs & money transfer

---
+++ app/components/Transfertrans.tsx
@@ -0,0 +1,82 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Card, 
+    CardContent, 
+    CardDescription, 
+    CardTitle, 
+    CardHeader,
+    CardFooter
+}   from "@/components/ui/card";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+export default function Transfertrans(){
+
+    return <Card
+    className="sm:col-span-2" x-chunk="dashboard-05-chunk-0"
+  >
+    <CardHeader className="pb-3">
+      <CardTitle>
+      </CardTitle>
+      <CardDescription className="text-balance max-w-lg leading-relaxed">
+      <Tabs defaultValue="Send" className="w-[400px]">
+      <TabsList className="grid w-full grid-cols-2">
+        <TabsTrigger value="Send">Send</TabsTrigger>
+        <TabsTrigger value="Receive">Receive</TabsTrigger>
+      </TabsList>
+      <TabsContent value="Send">
+        <Card>
+          <CardHeader>
+            <CardTitle>Send Money</CardTitle>
+            <CardDescription>
+              Send money to any userid.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="space-y-1">
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" defaultValue="Pedro Duarte" />
+            </div>
+            <div className="space-y-1">
+              <Label htmlFor="username">Username</Label>
+              <Input id="username" defaultValue="@peduarte" />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button>Save changes</Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+      <TabsContent value="Receive">
+        <Card>
+          <CardHeader>
+            <CardTitle>Password</CardTitle>
+            <CardDescription>
+              Change your password here. After saving, you'll be logged out.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2">
+            <div className="space-y-1">
+              <Label htmlFor="current">Current password</Label>
+              <Input id="current" type="password" />
+            </div>
+            <div className="space-y-1">
+              <Label htmlFor="new">New password</Label>
+              <Input id="new" type="password" />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button>Save password</Button>
+          </CardFooter>
+        </Card>
+      </TabsContent>
+    </Tabs>
+      </CardDescription>
+    </CardHeader>
+    <CardFooter>
+     
+    </CardFooter>
+  </Card>
+
+}
--- app/transfer/page.tsx
+++ app/transfer/page.tsx
@@ -3,6 +3,10 @@ import { redirect } from 'next/navigation'
 import Image from "next/image";
 import { useSession, signOut } from "next-auth/react"
 import Navbar from '../components/navbar';
+import Transfertrans from '../components/Transfertrans';
+import BalanceGraph from '../components/ui/BalanceGraph';
+import { Button } from '@/components/ui/button';
+import { Lock } from 'lucide-react';
 
 export default function Transfer() {
 const a= useSession();
@@ -16,7 +20,23 @@ if(a.status=="unauthenticated"){
     <Navbar
      selected="Transfer"
     />
-    ss
+    <div className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 lg:grid-cols-2">
+  <div className="flex flex-col p-4">
+  <h2 className="text-2xl p-1">Transfer Amount</h2>
+   <Transfertrans/>
+  </div>
+  <div className="flex flex-col p-12">
+    <center>
+    <BalanceGraph/>
+    <div className="m-10">
+    <Button>
+        <Lock className="h-6 w-6" />
+        <span className="sr-only sm:not-sr-only">Change PIN</span>
+    </Button>
+      </div>
+    </center>
+  </div>
+</div>
     </div>
     </main>
   );
