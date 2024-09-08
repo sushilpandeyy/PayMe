de13d8e1c0ca429cdea826917c06c363119e449c
Contactsushil <contact.sushilpandey@gmail.com>
2024-09-08 11:47:02 +0530

    Force Dynamics

--- app/api/p/transaction/route.ts
+++ app/api/p/transaction/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from '@prisma/client';
 import qs from 'qs';
+export const dynamic = 'force-dynamic';
 
 const prisma = new PrismaClient();
 
--- app/api/p/transaction/verify/route.ts
+++ app/api/p/transaction/verify/route.ts
@@ -1,28 +1,36 @@
 import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from '@prisma/client';
 
+// Force the API route to be dynamic
+export const dynamic = 'force-dynamic';
+
 const prisma = new PrismaClient();
 
 export async function GET(request: NextRequest) {
     try {
         const id = request.nextUrl.searchParams.get("id");
         const amtStr = request.nextUrl.searchParams.get("amt");
+
         if (!id) {
-            console.error("User ID is required")
+            console.error("User ID is required");
             return NextResponse.json({ message: 'User ID is required' }, { status: 401 });
         }
+
         if (!amtStr) {
             return NextResponse.json({ message: 'Amount is required' }, { status: 402 });
         }
+
         const amt = parseFloat(amtStr);
         if (isNaN(amt) || amt <= 0) {
             return NextResponse.json({ message: 'Invalid amount' }, { status: 400 });
         }
+
         const user = await prisma.account.findFirst({
             where: {
                 userID: id,
             },
         });
+
         if (user?.userID) {
             if (user.Balance >= amt) {
                 return NextResponse.json({ message: 'Low balance' }, { status: 201 });
